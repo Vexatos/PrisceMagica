cbffce3ed1cc796ab424875f8bef306b15b77da9
Vazkii <vasco.men@hotmail.com>
2013-08-06 21:24:26 +0100

    Initial code, mcmod.info and some git/eclipse stuff.

---
+++ MODSRC/vazkii/priscemagica/PrisceMagica.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the PrisceMagica Mod.
+ * 
+ * PrisceMagica is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [6 Aug 2013, 20:59:29 (GMT)]
+ */
+package vazkii.priscemagica;
+
+import vazkii.priscemagica.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class PrisceMagica {
+
+}
---
+++ MODSRC/vazkii/priscemagica/lib/LibMisc.java
@@ -0,0 +1,19 @@
+/**
+ * This class was created by <Vazkii>. It's distributed as
+ * part of the PrisceMagica Mod.
+ * 
+ * PrisceMagica is Open Source and distributed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License
+ * (http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_GB)
+ * 
+ * File Created @ [6 Aug 2013, 21:01:31 (GMT)]
+ */
+package vazkii.priscemagica.lib;
+
+public final class LibMisc {
+
+	public static final String MOD_ID = "PrisceMagica";
+	public static final String MOD_NAME = MOD_ID;
+	public static final String VERSION = "ANT:VERSION";
+	
+}
