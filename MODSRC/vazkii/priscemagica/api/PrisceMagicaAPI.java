f646b7c68edde0d278a85a94ad53e28b333063b3
Vazkii <vasco.men@hotmail.com>
2013-08-09 23:22:46 +0100

    Spell mappings. Very basic.

--- MODSRC/vazkii/priscemagica/api/PrisceMagicaAPI.java
+++ MODSRC/vazkii/priscemagica/api/PrisceMagicaAPI.java
@@ -10,6 +10,31 @@
  */
 package vazkii.priscemagica.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class PrisceMagicaAPI {
 
+	private static Map<String, ISpell> spells = new HashMap();
+	protected static Map<ISpell, SpellAvailability> availability = new HashMap();
+	
+	public void registerSpell(ISpell spell, String name) {
+		if(spells.containsKey(name))
+			throw new IllegalArgumentException("Name \"" + name + "\" is already taken by " + spells.get(name) + " when adding " + spell + "!");
+		
+		spells.put(name, spell);
+	}
+	
+	public ISpell getSpell(String name) {
+		return spells.get(name);
+	}
+	
+	public SpellAvailability getAvailability(String name) {
+		return getAvailability(getSpell(name));
+	}
+	
+	public SpellAvailability getAvailability(ISpell spell) {
+		return spell != null && availability.containsKey(spell) ? availability.get(spell) : SpellAvailability.DISABLED;
+	}
+	
 }
